8e657cba98e8c34cef2e70301c00dc37abfff50b
Andrew Bonnah <andrewbonnah@Andrews-MBP-218.hsd1.mi.comcast.net>
2025-03-30 20:23:05 -0400

    added new capa

--- handleresize.js
+++ handleresize.js
@@ -13,16 +13,19 @@ document.addEventListener("DOMContentLoaded", function() {
       }
 
       // Apply different styles based on window width
-      if (width < 768) {
-          console.log("Mobile view");
-          link.href = "css/mobile.css"; // Apply mobile styles
-      } else if (width >= 768 && width < 992) {
-          console.log("Tablet view");
-          link.href = "css/tablet.css"; // Apply tablet styles
-      } else {
-          console.log("Desktop view");
-          link.href = "css/desktop.css"; // Apply desktop styles
-      }
+      if (width < 768 && link.outerHTML !== '<link rel="stylesheet" id="dynamic-style" href="css/mobile.css">') {
+        console.log("Mobile view");
+        link.href = "css/mobile.css"; // Apply mobile styles
+        document.getElementById("year").innerHTML = new Date().getFullYear();
+    } else if (width >= 768 && width < 992 && link.outerHTML !== '<link rel="stylesheet" id="dynamic-style" href="css/tablet.css">') {
+        console.log("Tablet view");
+        link.href = "css/tablet.css"; // Apply tablet styles
+        document.getElementById("year").innerHTML = new Date().getFullYear();
+    } else if (width >= 992 && link.outerHTML !== '<link rel="stylesheet" id="dynamic-style" href="css/desktop.css">') {
+        console.log("Desktop view");
+        link.href = "css/desktop.css"; // Apply desktop styles
+        document.getElementById("year").innerHTML = new Date().toLocaleDateString();
+    }
   }
 
   // Initial check on page load
@@ -31,3 +34,24 @@ document.addEventListener("DOMContentLoaded", function() {
   // Event listener for window resize
   window.addEventListener("resize", handleResize);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const header = document.getElementById("headerText");
+  const button = document.getElementById("toggleButton");
+
+  if (header && button) {  // Check if the elements exist
+      let isLarge = false;
+
+      function toggleTextSize() {
+          if (isLarge) {
+              header.style.fontSize = "30px";
+          } else {
+              header.style.fontSize = "70px";
+          }
+          isLarge = !isLarge;
+      }
+
+      button.addEventListener("click", toggleTextSize);
+  }
+});
+
